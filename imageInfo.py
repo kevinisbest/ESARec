e0fcf16fd12ce7b3b81479585a3b4fc9d5be235e
SunnerLi <a6214123@gmail.com>
2015-10-01 10:51:43 +0800

    first commit

---
+++ imageInfo.py
@@ -0,0 +1,13 @@
+import sys
+
+class imageInfo(object):
+	"""
+		The class contain image & the coordinate of the button point
+	"""
+	imageFilePtr = None
+	xy = []
+
+	#constructoion
+	def __init__(self, ptr, point):
+		imageFilePtr = ptr
+		xy = Point
