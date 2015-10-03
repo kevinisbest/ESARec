92b667be2c0e7a009fb246facabec2c98062d521
SunnerLi <a6214123@gmail.com>
2015-10-03 16:15:41 +0800

    add phone short example

---
+++ D930_passing/app/src/main/java/com/example/sunner/d930_passing/Constants.java
@@ -0,0 +1,17 @@
+package com.example.sunner.d930_passing;
+
+/**
+ * Created by sunner on 2015/9/30.
+ */
+public class Constants {
+    public static final String LLOG = "log資訊";  //log
+    public static final int PORT_NUMBER = 10017;
+    public static final String IMAGE1_ADDR = "sdcard/DCIM/000004.jpg";   //檔案1完整路徑
+    public static final String IMAGE2_ADDR = "sdcard/DCIM/001551.jpg";   //檔案2完整路徑
+
+
+    //命令
+    public static final String START_TO_SEND_IMAGE = "0100";
+    public static final String END_SEND_IMAGE = "0101";
+
+}
